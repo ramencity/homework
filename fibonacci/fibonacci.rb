8b4dcfe09a5cc847d5d0e0218589151f2760030a
ramencity <plutopay@gmail.com>
2015-10-12 14:12:27 -0700

    added closest_fibonacci_to method, added unit tests; extracated the type-checking into a helper function and moved the helpers to the top of the file

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -1,10 +1,25 @@
+# add a given number to an array and return the new number by adding the last two elements of the array
+# @param array [Array] of fibonacci numbers
+# @param number [Fixnum] to add to array
+# @return [Fixnum]
+def add_to_fibonacci_array(array, number)
+  array.push(number)
+  array[-1] + array[-2]
+end
+
+# raise an exception if the value passed in is not a Fixnum
+# @param x [Fixnum]
+def check_is_fixnum(x)
+  unless x.is_a?(Fixnum)
+    raise TypeError, "'#{x}' is not a Fixnum; try again"
+  end
+end
+
 # give the range of fibonacci numbers up to and including a final value x
 # @param x [Integer]
 # @return [Array]
 def fibonacci(x)
-  unless x.is_a?(Fixnum)
-    raise TypeError, "'#{x}' is not a Fixnum; try again"
-  end
+  check_is_fixnum(x)
 
   a = [1]
   new_number = 1
@@ -18,9 +33,7 @@ end
 # @param y [Integer]
 # @return [Array]
 def fibonacci_size(y)
-  unless y.is_a?(Fixnum)
-    raise TypeError, "'#{y}' is not a Fixnum; try again"
-  end
+  check_is_fixnum(y)
 
   a = [1]
   new_number = 1
@@ -31,11 +44,16 @@ def fibonacci_size(y)
   a
 end
 
-# add a given number to an array and return the new number by adding the last two elements of the array
-# @param array [Array] of fibonacci numbers
-# @param number [Fixnum] to add to array
+# return the closest number (equal to or lesser than) a given cap of z
+# @param z [Fixnum]
 # @return [Fixnum]
-def add_to_fibonacci_array(array, number)
-  array.push(number)
-  array[-1] + array[-2]
+def closest_fibonacci_to(z)
+  check_is_fixnum(z)
+
+  a = [1]
+  new_number = 1
+  while new_number <= z
+    new_number = add_to_fibonacci_array(a, new_number)
+  end
+  a.last
 end

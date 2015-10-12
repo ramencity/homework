55ce5c2530ea9df5462418d985d26e1321600f6a
ramencity <plutopay@gmail.com>
2015-10-12 13:05:30 -0700

    added additional method to return an array of a given size, rather than having a given highest value; added unit tests for said method

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -14,3 +14,21 @@ def fibonacci(x)
   end
   a
 end
+
+# return an array of fibonacci numbers that is a given size 'y'
+# @param y [Integer]
+# @return [Array]
+def fibonacci_size(y)
+  unless y.is_a?(Fixnum)
+    raise TypeError, "'#{y}' is not a Fixnum; try again"
+  end
+
+  a = [1]
+  new_number = 1
+  #since we're seeding the array to start out with one element, remove that from the number of times to loop:
+  (y-1).times do
+    a.push(new_number)
+    new_number = a[-1] + a[-2]
+  end
+  a
+end
--- fibonacci/test_fibonacci.rb
+++ fibonacci/test_fibonacci.rb
@@ -3,9 +3,14 @@ require 'test/unit'
 
 class TestFibonacci < Test::Unit::TestCase
 
-  def test_fibonacci_array
+  def test_fibonacci_array_final_value
     assert_equal([1, 1, 2, 3, 5, 8, 13], fibonacci(20) )
     assert_equal([1, 1, 2, 3, 5, 8, 13, 21], fibonacci(21))
+  end
+
+  def test_fibonacci_array_size
+    assert_equal(20, fibonacci_size(20).size)
+    assert_equal(1, fibonacci_size(1).size)
   end
 
 end

faf41d875f539402bc5350375d9eec602ee458b3
ramencity <plutopay@gmail.com>
2015-10-12 13:42:10 -0700

    adding grumpy-path unit tests for strings rather than ints passed in

--- fibonacci/test_fibonacci.rb
+++ fibonacci/test_fibonacci.rb
@@ -16,4 +16,9 @@ class TestFibonacci < Test::Unit::TestCase
   def test_array_add_method
     assert_equal(13, add_to_fibonacci_array([1, 1, 2, 3, 5], 8))
   end
+
+  def test_non_numbers_raise_error
+    assert_raise(TypeError) {fibonacci('x')}
+    assert_raise(TypeError) {fibonacci_size('x')}
+  end
 end
